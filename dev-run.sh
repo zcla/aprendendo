42237ea97310d93deb844f6cff36076aedd0e117
P_6411 <jose.aguiar@camara.leg.br>
2021-09-13 14:41:21 -0300

    python/dev: configuração inicial

--- dev-run.sh
+++ dev-run.sh
@@ -1,3 +1,3 @@
 docker-compose down
-docker-compose build
+docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml build
 docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d
